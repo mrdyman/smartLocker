d2c5d110edd0948f723b2c78a37cc0e433bd2ec2
mrdyman <andimardimansaputra@gmail.com>
2021-07-27 21:34:54 +0800

    add new fragment

--- app/src/main/java/com/macca/smartlocker/MainActivity.kt
+++ app/src/main/java/com/macca/smartlocker/MainActivity.kt
@@ -1,34 +1,48 @@
 package com.macca.smartlocker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.macca.smartlocker.Fragment.HomeFragment
+import com.macca.smartlocker.Fragment.LockerFragment
+import com.macca.smartlocker.Fragment.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bn_home.setOnNavigationItemSelectedListener(bottomNavListener)
+
         val fr = supportFragmentManager.beginTransaction()
         fr.add(R.id.ll_locker_container, HomeFragment())
         fr.commit()
+    }
 
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.home -> {
-                    // Respond to navigation home click
-                    Log.d("Bnav", "Home")
-                    true
-                }
-                R.id.mylocker -> {
-                    // Respond to navigation My Locker click
-                    Log.d("Bnav", "My Locker")
-                    true
-                }
-                else -> false
+    private val bottomNavListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFr: Fragment = HomeFragment()
+        when (item.itemId) {
+            R.id.nav_home -> {
+                selectedFr = HomeFragment()
+            }
+            R.id.nav_mylocker -> {
+                selectedFr = LockerFragment()
+            }
+            R.id.nav_history -> {
+                selectedFr = HomeFragment()
+            }
+            R.id.nav_profile -> {
+                selectedFr = ProfileFragment()
             }
         }
+
+        val fr = supportFragmentManager.beginTransaction()
+        fr.replace(R.id.ll_locker_container, selectedFr)
+        fr.commit()
+        true
     }
 }
