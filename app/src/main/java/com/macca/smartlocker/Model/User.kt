9997fe9d7bf64453126f3db5d71150694ea4eea3
mrdyman <andimardimansaputra@gmail.com>
2021-07-28 17:26:04 +0800

    create registration function

--- app/src/main/java/com/macca/smartlocker/Model/User.kt
+++ app/src/main/java/com/macca/smartlocker/Model/User.kt
@@ -1,3 +1,3 @@
 package com.macca.smartlocker.Model
 
-data class User(val nama_lengkap : String? = null, val email : String? = null, val alamat : String? = null, val kodePos: String? = null)
+data class User(val nama_lengkap : String? = null, val email : String? = null, val alamat : String? = null, val kodePos: String? = null, val User_Id : String? = null)
--- app/src/main/java/com/macca/smartlocker/RegistrationActivity.kt
+++ app/src/main/java/com/macca/smartlocker/RegistrationActivity.kt
@@ -3,7 +3,9 @@ package com.macca.smartlocker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.macca.smartlocker.Model.User
@@ -19,6 +21,7 @@ class RegistrationActivity : AppCompatActivity() {
 
         btn_signup.setOnClickListener {
 
+            //ambil data user yang di input
             val nama = et_nama.text.toString()
             val email = et_email.text.toString()
             val alamat = et_alamat.text.toString()
@@ -26,20 +29,42 @@ class RegistrationActivity : AppCompatActivity() {
             val password = et_pass.text.toString()
             val password1 = et_pass1.text.toString()
 
-            database = FirebaseDatabase.getInstance("https://smartlocker-7f844-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
-            val user = User(nama, email, alamat, kodePos)
-            database.child(nama).setValue(user).addOnSuccessListener {
-                et_nama.text.clear()
-                et_email.text.clear()
-                et_alamat.text.clear()
-                et_kode_pos.text.clear()
-                et_pass.text.clear()
-                et_pass1.text.clear()
+            //create akun user(insert data ke firebase authentication)
+            val auth = FirebaseAuth.getInstance()
+//            var userId : String? = null
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful){
+                        //ambil user id yang di generate
+                        val userId = auth.currentUser?.uid
 
-                Toast.makeText(this, "Successfully Created Data", Toast.LENGTH_SHORT).show()
-            } .addOnFailureListener {
-                Toast.makeText(this, "Failed Created Data", Toast.LENGTH_SHORT).show()
-            }
+                        //tampilkan pesan sukses registrasi akun baru
+                        Toast.makeText(this, "Account Successful created.", Toast.LENGTH_SHORT).show()
+
+                        //insert data user ke database
+                        database = FirebaseDatabase.getInstance("https://smartlocker-7f844-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
+                        val user = User(nama, email, alamat, kodePos, userId.toString())
+                        database.child(nama).setValue(user).addOnSuccessListener {
+
+                            //clear field registrasi
+                            et_nama.text.clear()
+                            et_email.text.clear()
+                            et_alamat.text.clear()
+                            et_kode_pos.text.clear()
+                            et_pass.text.clear()
+                            et_pass1.text.clear()
+
+                            Toast.makeText(this, "Successfully Created Data", Toast.LENGTH_SHORT).show()
+                        } .addOnFailureListener {
+                            Toast.makeText(this, "Failed Created Data", Toast.LENGTH_SHORT).show()
+                        }
+
+
+                    } else {
+                        Log.d("Failed_Ex", "${it.result}")
+                        Toast.makeText(this, "Account failed to created. with meesage ${it.exception?.printStackTrace()}", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
             val i = Intent(this, LoginActivity::class.java)
             startActivity(i)
