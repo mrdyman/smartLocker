81e7c4c578e7b469413a5438a9258ffe1b56ea37
mrdyman <andimardimansaputra@gmail.com>
2021-08-02 07:26:45 +0800

    add function getUserData to fragment profile

--- app/src/main/java/com/macca/smartlocker/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/macca/smartlocker/Fragment/ProfileFragment.kt
@@ -7,13 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.macca.smartlocker.LoginActivity
 import com.macca.smartlocker.MainActivity
 import com.macca.smartlocker.R
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var databaseReferenceUser : DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -28,5 +32,41 @@ class ProfileFragment : Fragment() {
             val i = Intent(requireActivity(), LoginActivity::class.java)
             startActivity(i)
         }
+        getUserData()
+    }
+
+    private fun getUserData() {
+        auth = FirebaseAuth.getInstance()
+        val userId = auth.currentUser?.uid
+        databaseReferenceUser = FirebaseDatabase.getInstance("https://smartlocker-7f844-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
+
+        databaseReferenceUser.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(user: DataSnapshot) {
+                if (user.exists()){
+                    for (mUser in user.children){
+                        val mUserId = mUser.child("user_Id").value
+                        if (userId.toString() == mUserId.toString()){
+                            val namaUser = mUser.child("nama_lengkap").value
+                            val alamat = mUser.child("alamat").value
+                            val email = mUser.child("email").value
+                            val kodePos = mUser.child("kodePos").value
+
+                            //assign data dari firebase ke textview profile
+                            tv_user_login_name_profile.text = namaUser.toString()
+                            tv_user_login_address_profile.text = alamat.toString()
+                            tv_profile_nama.text = namaUser.toString()
+                            tv_profile_email.text = email.toString()
+                            tv_profile_address.text = alamat.toString()
+                            tv_profile_kode_pos.text = kodePos.toString()
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(dataBaseError: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
